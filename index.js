48156cb40d9af656f3999d41d12b28dfb339b0d0
OmniaGomaa88 <73542760+OmniaGomaa88@users.noreply.github.com>
2021-03-02 18:44:50 +0100

    final modification with commintairs

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-let box =Array.from(document.querySelectorAll("button"))
-let textdiv = document.querySelector("#game_status")
+let box =Array.from(document.querySelectorAll("button"))  // array of all the buttons
+let textdiv = document.querySelector("#game_status") // the text down of all the buttons
 
 
 const winningCombinations = [
-    // array of 
+    // array of all the cases of wins
     [box[0], box[1], box[2]],
     [box[3], box[4], box[5]],
     [box[6], box[7], box[8]],
@@ -13,37 +13,46 @@ const winningCombinations = [
     [box[0], box[4], box[8]],
     [box[2], box[4], box[6]]
 ]
-
+// function for cheking how is the winner
 function winner(){
+     // chicking of the cases
     for(let i=0;i<winningCombinations.length;i++){
         if(winningCombinations[i][0].innerHTML===winningCombinations[i][1].innerHTML&&
              winningCombinations[i][1].innerHTML===winningCombinations[i][2].innerHTML
             &&(winningCombinations[i][0].innerHTML!==""))
         {
-            winningCombinations[i][0].style.backgroundColor="green"
-            winningCombinations[i][1].style.backgroundColor="green"
-            winningCombinations[i][2].style.backgroundColor="green"
+            // change the colors  of all the boxs of the  winner
+            winningCombinations[i][0].style.backgroundColor="lightGreen"
+            winningCombinations[i][1].style.backgroundColor="lightGreen"
+            winningCombinations[i][2].style.backgroundColor="lightGreen"
+             // show the winner name
             textdiv.textContent="the winner is"+" "+winningCombinations[i][0].innerHTML
+            // remove click from each button in the end of the game
+            box.forEach(winner=>{
+                winner.removeEventListener('click',changerLeText)
+            })
         }
         
 }
 }
 
-// donn le text X O
+//show the X or O by clikcing
 let turne=true
 function changerLeText(e){
     if(turne){
         e.target.innerHTML="X"
         textdiv.textContent= "is O turn"
-        e.target.removeEventListener('click',changerLeText )// remove click 
+        e.target.removeEventListener('click',changerLeText )// remove click from each button if it not the playes trune
         turne=false
     }else{
         e.target.innerHTML="O"
         textdiv.textContent= "is X turn"
-        e.target.removeEventListener('click',changerLeText );// remove click 
+        e.target.removeEventListener('click',changerLeText );// remove click from each button if it not the playes trune
         turne=true
     }
-    winner()
+    
+    winner()// calling the function winner 
+    
 }
 
 box.forEach(button=>{
